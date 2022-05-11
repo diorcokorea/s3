8a0a11789273075488de57d854f70f0319ed817b
diorcokorea <yknam@diorco.co.kr>
2022-05-11 10:24:00 +0900

    2nd push

--- server.js
+++ server.js
@@ -6,9 +6,8 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json());
-// parse requests of content-type - application/json
-app.use(express.json());
+app.use(express.json({ limit: "150mb" }));
+app.use(express.urlencoded({ limit: "150mb", extended: false }));
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
